6c805dc6aac112727f73ad5ee29fa9f5fdd9cf54
Ayoub Nejjari <raysoflee07@gmail.com>
2017-05-28 12:03:33 +0000

    adding basic error handling in the server

--- app.js
+++ app.js
@@ -6,10 +6,23 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 
 
-// 404 custom response middleware 
+// 404 custom response middleware
 
 app.use(function(req, res){
 	res.type('text/plain');
 	res.status(404);
 	res.send('404 - Not found');
+});
+
+app.use(function(req, res) {
+	console.log(err.stack);
+	res.type('text/plain');
+	res.status(500);
+	res.send('500 - Internal Error');
+
+});
+
+
+app.listen(app.get('port'), function () {
+	console.log('Express started on port ' + app.get('port'));
 });
